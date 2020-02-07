b4ebf96e7513cf69653923c0e8e003f52446f977
Hanna Lee <mani21na@gmail.com>
2020-02-07 04:46:27 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,9 @@
 class Song < ApplicationRecord
+  include ActiveModel::Validations
   validates :title, presence: true, uniqueness: { scope: :year }
   validates :released, inclusion: { in: %w[true false] }
   validates :artist_name, presence: true
   validates :release_year, presence: true, unless: -> { released.value == false }
-  validate release_year_cannot_be_in_the_past:
+  validates_with ReleaseYearValidator
 
-
-  def release_year_cannot_be_in_the_past
-    if release_year > Date.current.year
-      errors.add(:release_year, "can't be in the past")
-    end
-  end
 end
