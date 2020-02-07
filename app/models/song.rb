e3b7c5ea796b18c77837a903a1f39a100104696d
Hanna Lee <mani21na@gmail.com>
2020-02-07 04:41:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,8 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :year }
   validates :released, inclusion: { in: %w[true false] }
   validates :artist_name, presence: true
-  validates :release_year, presence: true, unless: -> { released.value == false }, release_year_cannot_be_in_the_past:
+  validates :release_year, presence: true, unless: -> { released.value == false }
+  validate release_year_cannot_be_in_the_past:
 
 
   def release_year_cannot_be_in_the_past
